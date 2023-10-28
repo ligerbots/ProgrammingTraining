d5c77aa030eb6318fb54013ad48d01fb41e0580b
WelchJ <jack_weq@icloud.com>
2023-10-28 01:05:20 -0400

    try

--- src/main/java/frc/robot/subsystems/NEOMotor.java
+++ src/main/java/frc/robot/subsystems/NEOMotor.java
@@ -6,45 +6,31 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class NEOMotor extends SubsystemBase {
   private final CANSparkMax m_motor;
   private final RelativeEncoder m_encoder;  
-  private final SparkMaxPIDController m_PIDController;
 
-  // PID Constants for the Neo motor PID controller
-  // Set PID proportional value to produce non-zero correction value when robot veers off
-  // straight line. P value controls how sensitive the correction is.
-  private static final double REACHER_K_P = 0.05;
-  private static final double REACHER_K_I = 0.0;
-  private static final double REACHER_K_D = 0.0;
-  private static final double REACHER_K_FF = 0.0;
-  
   /** Creates a new NEOMotor. */
   public NEOMotor() {
-    // create motor controller Sparkmax, assign CANID 1 to m_motor, type of motor = brushelss
+    // create motor controller Sparkmax, assign CANID to m_motor, type of motor = brushelss
     m_motor = new CANSparkMax(Constants.MOTOR_CAN_ID, MotorType.kBrushless);
-    m_motor.restoreFactoryDefaults();
+    m_motor.restoreFactoryDefaults(); 
 
     // assigns m_encoder to m_motor
     m_encoder = m_motor.getEncoder();
 
-    // assigns PID controller to motor
-    m_PIDController = m_motor.getPIDController();
-    m_PIDController.setP(REACHER_K_P);
-    m_PIDController.setI(REACHER_K_I);
-    m_PIDController.setD(REACHER_K_D);
-    m_PIDController.setFF(REACHER_K_FF);
+    // Set the position conversion factor.
+    // m_encoder.setPositionConversionFactor(1.0);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-
-    // should i put anything here?
+    m_motor.set(0.5);
   }
 }
